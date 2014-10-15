3cfab6e96fc9f859a8ccc2c686949c62c5734595
Amaras <amaras@vivaldi.net>
2014-10-15 17:58:00 +0200

    Ajout table SQL pour datalog

---
+++ MySQL/datalog_meteo.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 15 Octobre 2014 à 17:56
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `tpe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datalog_meteo`
+--
+
+DROP TABLE IF EXISTS `datalog_meteo`;
+CREATE TABLE IF NOT EXISTS `datalog_meteo` (
+  `temps` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `temperature` decimal(3,2) DEFAULT NULL,
+  `humidite` decimal(3,2) DEFAULT NULL,
+  PRIMARY KEY (`temps`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
