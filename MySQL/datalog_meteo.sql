80106b5a1c28d1549e0f48b808ee402a1d3e5db9
Amaras <amaras@vivaldi.net>
2014-10-20 18:28:15 +0200

    Supression de commentaires inutiles

--- MySQL/datalog_meteo.sql
+++ MySQL/datalog_meteo.sql
@@ -1,8 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
 -- Généré le :  Sam 18 Octobre 2014 à 18:11
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
@@ -11,17 +6,10 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Base de données :  `tpe`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Structure de la table `datalog_meteo`
 --
@@ -41,9 +29,5 @@ CREATE TABLE IF NOT EXISTS `datalog_meteo` (
 -- Contenu de la table `datalog_meteo`
 --
 
-INSERT INTO `datalog_meteo` (`id`, `temps`, `temperature`, `humidite`) VALUES
-(1, '2014-10-18 16:09:17', 105, 100);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `datalog_meteo` (`temps`, `temperature`, `humidite`) VALUES
+('2014-10-18 16:09:17', 105, 100);
